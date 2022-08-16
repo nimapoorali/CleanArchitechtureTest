3d293fffba3fcd7dc7104d9086707b96c4f74f81
nima, poorali <poorali.q.n@gmail.com>
2022-08-16 22:16:00 +0430

    Created ValidationBehavior and ServiceCollection Dependency injections

---
+++ Test.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Application.Behaviors;
+
+namespace Test.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCustomerCore(this IServiceCollection services)
+        {
+            services
+                .AddMediatR(Assembly.GetExecutingAssembly())
+                .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
