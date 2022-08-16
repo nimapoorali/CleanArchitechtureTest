737e6ba8add96ee4e3029a88b79ec955448dff69
nima, poorali <poorali.q.n@gmail.com>
2022-08-16 13:53:03 +0430

    Created ICustomerDBContext

---
+++ Test.Application/Abstractions/ICustomerDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Entities;
+
+namespace Test.Application.Abstractions
+{
+    internal interface ICustomerDBContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
